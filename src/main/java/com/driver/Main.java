6ce404e4316d08aee71b4b40fb972a50ab718bb3
Ajey <ajcorppvtltd@gmail.com>
2024-02-25 14:30:33 +0530

    changes commiteddvvv

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,11 +5,11 @@ public class Main {
 
     public static void main(String[] args) {
         Product p = new Product();
-         int ans =p.product(10,20);
+         int ans =p.product(13,70);
 
-         int ans1 = p.product(1,2,3);
+         int ans1 = p.product(5,7,11);
 
-         double ans2 = p.product(1.0,2.0);
+         double ans2 = p.product(17.0,28.0);
 
          System.out.println(ans +" "+ans1+" "+ans2);
     }
